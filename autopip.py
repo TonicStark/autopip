d8ffff65f62d9cd1a24fa956de9f797b4b262a33
francesco <francescosacco.github@gmail.com>
2023-01-22 11:25:57 +0100

    Implemented Path to Project Input

--- autopip.py
+++ autopip.py
@@ -3,3 +3,15 @@ from rich.console import Console
 import subprocess
 import sys
 import os
+
+# Global Console Object
+console = Console()
+
+# Getting the Project Path from Command Line
+try:
+    PRJ_PATH = sys.argv[1]
+except IndexError:
+
+    # Raising an Error Message
+    console.print(
+        "❌ [red]Error:[/red] you must use autopip [magenta]<.../to/project>[/magenta]!")
